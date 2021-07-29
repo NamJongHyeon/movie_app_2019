deb201f71c198ceff192f9a2b12155950d377c81
NamJongHyeon <crjh8942@gmail.com>
2021-07-29 17:08:26 +0900

    ch2 update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import React from "react";
+// import Potato from "./Potato"; // Potato 가져오기
+
+import PropTypes from "prop-types";
+
+function Food({ name, picture, rating }) { // react로 prop 전달
+  return <div>
+    <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src= {picture}/>
+  </div>// App에서 fav 값을 통해 나타냄
+}
+
+Food.propTypes = { // proptype으로 맞는 데이터 가져왔는지 check!
+  name: PropTypes.string.isRequired,
+  picture:PropTypes.string.isRequired,
+  rating:PropTypes.number
+}
+
+const foodILike = [
+  {
+    id:1,
+    name: "Kimchi",
+    image: "https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20210708_150%2F16257237667396pBLp_JPEG%2F26859550445256956_788994864.jpg&type=sc960_832",
+
+  },
+  {
+    id:2,
+    name: "Kimbob",
+    image: "https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20210701_56%2F1625106603144C1jNk_JPEG%2F26242437980847697_1247728794.jpg&type=sc960_832",
+ 
+  }
+];
 
 function App() {
-  return <div>
-          <h1>Hello!!!!!</h1>
-        </div>;
+  return ( 
+    <div>
+      {/* <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" /> */}
+      {foodILike.map(dish => (
+        <Food 
+        key = {dish.id} 
+        name={dish.name} 
+        picture={dish.image} 
+        rating={dish.rating} /> 
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />, // component, react는 하나의 component만 가져올 수 있음
   document.getElementById('root') //root에 요소를 넣어줌
 );
 
